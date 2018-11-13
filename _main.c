e16b4ce7f891d1ff7f38d8f98d256a0588e59d0d
qubitAI <osmanmorales@ymail.com>
2018-11-13 01:48:31 -0500

    Update _main.c

--- _main.c
+++ _main.c
@@ -85,95 +85,98 @@ int enlarge(PIXEL* original, int rows, int cols, int scale,
  * newrows  - the new number of rows
  * newcols  - the new number of cols
  */
+/*
+ * This method rotates a 24-bit, uncompressed .bmp file that has been read
+ * in using readFile(). The rotation is expressed in degrees and can be
+ * positive, negative, or 0 -- but it must be a multiple of 90 degrees
+ *
+ * original - an array containing the original PIXELs, 3 bytes per each
+ * rows     - the number of rows
+ * cols     - the number of columns
+ * rotation - a positive or negative rotation,
+ *
+ * new      - the new array containing the PIXELs, allocated within
+ * newrows  - the new number of rows
+ * newcols  - the new number of cols
+ */
 int rotate(PIXEL* original, int rows, int cols, int rotation,
 	   PIXEL** new, int* newrows, int* newcols)
 {
   /* THIS IS THE METHOD THAT YOU SHOULD WRITE */
   int c;
   int r;
-//int option;
-	switch(rotation)	
-    {
-		case 360:
-		  *newrows = rows;
-		  *newcols = cols;
-		  *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-	
-		for (r=0; r < rows; r++)
-		{
-		
-		  for (c = 0; c<cols; c++)
-		  {
-			PIXEL* o = original + r * cols + c;
-			PIXEL* n = (*new) + r*cols + c;
-			*n=*o;
-		  }
-	        }
-		break;
-		case 270:
-			  
-		    *newrows = cols;
-                    *newcols = rows;
-                    *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-			for (r=0; r < rows; r++)
-			{
-			  for (c=0; c < cols; c++) 
-			  {
-                            PIXEL* o = original + r*cols + c;
-                            PIXEL* n = (*new) + (cols-c-1)*rows + r;
-			    *n = *o;
-				  }
-			}
+	  
+  //int option;
+  switch(rotation)	
+  {
+    case 360:
+      *newrows = rows;
+      *newcols = cols;
+      *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+    
+      for (r=0; r < rows; r++)
+      {
+      	
+        for (c = 0; c<cols; c++)
+        {
+          PIXEL* o = original + r * cols + c;
+	  PIXEL* n = (*new) + r*cols + c;
+	  *n=*o;
+        }
+      }
+    break;
+    case 270:
+      			  
+      *newrows = cols;
+      *newcols = rows;
+      *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+      for (r=0; r < rows; r++)
+      {
+        for (c=0; c < cols; c++) 
+  	{
+           PIXEL* o = original + r*cols + c;
+           PIXEL* n = (*new) + (cols-c-1)*rows + r;
+	   *n = *o;
+	}
+      }
 		  
-		break;
-		case 180:
-		    *newrows = cols;
-                    *newcols = rows;
-                    *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-			for (r=0; r < rows; r++)
-			{
-			  for (c=0; c < cols; c++) 
-			  {
-
-                            PIXEL* o = original + r * cols + c;
-                            PIXEL* n = (*new) + (rows-r)*cols - (c+1);
-			    *n = *o;
-			  }
-			//}
-		  //}
-		break;
-		case 90:
-		    *newrows = cols;
-                    *newcols = rows;
-                    *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-			if ('-')
-			{
-				
-			}
-			else
-			{
-				
-				
-			}
-			for (r=0; r < rows; r++)
-			{
-			  for (c=0; c < cols; c++) 
-			  {
-
-                            PIXEL* o = original + r*cols + c;
-                            PIXEL* n = (*new) + (cols-c-1)*rows + r;
-			    *n = *o;
-			  }
-			}
-		  
-		break;
-		  }
+    break;
+    case 180:
+      *newrows = cols;
+      *newcols = rows;
+      *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+      for (r=0; r < rows; r++)
+      {
+        for (c=0; c < cols; c++) 
+	{
+ 
+          PIXEL* o = original + r * cols + c;
+          PIXEL* n = (*new) + (rows-r)*cols - (c+1);
+	  *n = *o;
+	}
+      }
+      break;
+      case 90:
+        *newrows = cols;
+        *newcols = rows;
+        *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+	for (r=0; r < rows; r++)
+	{
+          for (c=0; c < cols; c++) 
+	  {
+         
+            PIXEL* o = original + r*cols + c;
+            PIXEL* n = (*new) + (cols-c-1)*rows + r;
+	    *n = *o;
+           }
+	}		  
+      break;
+  }
 	  
   
   return 0;
-	}
+}
 	
-	}
 /*
  * This method rotates a 24-bit, uncompressed .bmp file that has been read
  * in using readFile(). The rotation is expressed in degrees and can be
@@ -193,91 +196,60 @@ int rotate(PIXEL* original, int rows, int cols, int rotation,
 int rotatenegative(PIXEL* original, int rows, int cols, int rotation,
 	   PIXEL** new, int* newrows, int* newcols)
 {
-  /* THIS IS THE METHOD THAT YOU SHOULD WRITE */
+  //columns and rows
   int c;
   int r;
 	  
 	//int option;
-	switch(rotation)	
-    {
-		case 360:
-		  *newrows = rows;
-		  *newcols = cols;
-		  *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-	
-		for (r=0; r < rows; r++)
-		{
-		
-		  for (c = 0; c<cols; c++)
-		  {
-			PIXEL* o = original + r * cols + c;
-			PIXEL* n = (*new) + r*cols + c;
-			*n=*o;
-		  }
-	        }
-		break;
-		case -270:
-			  
-		    *newrows = cols;
-                    *newcols = rows;
-                    *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-			for (r=0; r < rows; r++)
-			{
-			  for (c=0; c < cols; c++) 
-			  {
-                            PIXEL* o = original + r*cols + c;
-                            PIXEL* n = (*new) + (cols-c-1)*rows + r;
-			    *n = *o;
-			  }
-			}
+  switch(rotation)	
+  {
+    case -270:			  
+      *newrows = cols;
+      *newcols = rows;
+      *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+       for (r=0; r < rows; r++)
+       {
+         for (c=0; c < cols; c++) 
+         {
+         PIXEL* o = original + r*cols + c;
+         PIXEL* n = (*new) + (cols-c-1)*rows + r;
+	 *n = *o;
+         }
+       }
+    break;
+    case -180:
+      *newrows = cols;
+      *newcols = rows;
+      *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+      for (r=0; r < rows; r++)
+      {
+        for (c=0; c < cols; c++) 
+	{
+          PIXEL* o = original + r * cols + c;
+          PIXEL* n = (*new) + (rows-r)*cols - (c+1);
+	  *n = *o;
+        }
+      }
+    break;
+    case -90:
+      *newrows = cols;
+      *newcols = rows;
+      *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
+      for (r=0; r < rows; r++)
+      {
+        for (c=0; c < cols; c++) 
+	{
+          PIXEL* o = original + r*cols + c;
+          PIXEL* n = (*new) + ((cols-c-1)*rows + r) * -1;
+	  *n = *o;
+	}
+      }
 		  
-		break;
-		case -180:
-		    *newrows = cols;
-                    *newcols = rows;
-                    *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-			for (r=0; r < rows; r++)
-			{
-			  for (c=0; c < cols; c++) 
-			  {
-
-                            PIXEL* o = original + r * cols + c;
-                            PIXEL* n = (*new) + (rows-r)*cols - (c+1);
-			    *n = *o;
-			  }
-				break;
-		case -90:
-		    *newrows = cols;
-                    *newcols = rows;
-                    *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
-			if ('-')
-			{
-				
-			}
-			else
-			{
-				
-				
-			}
-			for (r=0; r < rows; r++)
-			{
-			  for (c=0; c < cols; c++) 
-			  {
-
-                            PIXEL* o = original + r*cols + c;
-                            PIXEL* n = (*new) + (cols-c-1)*rows + r;
-			    *n = *o;
-			  }
-			}
-		  
-		break;
-		  }
-	  
-  
+    break;
+ 
   return 0;
-	}
-	
-	}
+  }
+}
 
 /*
  * This method Vertically flips a 24-bit, uncompressed bmp file
