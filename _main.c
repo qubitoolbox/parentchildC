82ca6f0d29339d90346221b582273fb82b9cda68
qubitAI <osmanmorales@ymail.com>
2018-11-14 11:16:49 -0500

    Update _main.c

--- _main.c
+++ _main.c
@@ -23,48 +23,33 @@ int enlarge(PIXEL* original, int rows, int cols, int scale,
 	    PIXEL** new, int* newrows, int* newcols)
 {
   /* THIS IS THE METHOD THAT YOU SHOULD WRITE */
-  
+  //rows and columns 
   int r;
   int c;
-  
+  //pointers of the new img
   *newrows = scale * rows;
   *newcols = scale * cols;
-  
   *new = (PIXEL*)malloc((*newrows)*(*newcols)*sizeof(PIXEL));
   
   for (int row=0; row<rows; row++)
   {
     for (int i=0; i<scale; i++)
-	{
+    {
       for (int col=0; col<cols; col++)
-	  {
-	    for(int j=0; j<scale; j++)
-		{
-			PIXEL* o = original+row*cols+col;
-			PIXEL* n = (*new) + row*cols*scale*scale + cols*scale*i + col*scale + j;
-			*n = *o;
-		}
-	  }
+      {
+        for(int j=0; j<scale; j++)
+	{
+	  PIXEL* o = original+row*cols+col;
+	  PIXEL* n = (*new) + row*cols*scale*scale + cols*scale*i + col*scale + j;
+	  *n = *o;
 	}
-	    
+      }
+    }	    
   }
   
   return 0;
 }
-/*
- * This method rotates a 24-bit, uncompressed .bmp file that has been read
- * in using readFile(). The rotation is expressed in degrees and can be
- * positive, negative, or 0 -- but it must be a multiple of 90 degrees
- *
- * original - an array containing the original PIXELs, 3 bytes per each
- * rows     - the number of rows
- * cols     - the number of columns
- * rotation - a positive or negative rotation,
- *
- * new      - the new array containing the PIXELs, allocated within
- * newrows  - the new number of rows
- * newcols  - the new number of cols
- */
+
 /*
  * This method rotates a 24-bit, uncompressed .bmp file that has been read
  * in using readFile(). The rotation is expressed in degrees and can be
@@ -192,7 +177,7 @@ int rotatenegative(PIXEL* original, int rows, int cols, int rotation,
          for (c=0; c < cols; c++) 
          {
          PIXEL* o = original + r*cols + c;
-         PIXEL* n = (*new) + (cols-c-1)*rows + r;
+         PIXEL* n = (*new) + (cols+c+1)*rows - r;
 	 *n = *o;
          }
        }
@@ -206,7 +191,7 @@ int rotatenegative(PIXEL* original, int rows, int cols, int rotation,
         for (c=0; c < cols; c++) 
 	{
           PIXEL* o = original + r * cols + c;
-          PIXEL* n = (*new) + (rows-r)*cols - (c+1);
+          PIXEL* n = (*new) + ((rows-r-1)*cols + c);
 	  *n = *o;
         }
       }
@@ -220,7 +205,7 @@ int rotatenegative(PIXEL* original, int rows, int cols, int rotation,
         for (c=0; c < cols; c++) 
 	{
           PIXEL* o = original + r*cols + c;
-          PIXEL* n = (*new) + ((cols-c-1)*rows + r) * -1;
+          PIXEL* n = (*new) + ((rows-r-1)*cols+ (c-1));
 	  *n = *o;
 	}
       }
@@ -253,14 +238,17 @@ int verticalflip (PIXEL *original, PIXEL **new, int rows, int cols)
   *new = (PIXEL*)malloc(rows*cols*sizeof(PIXEL));
 
   for (row=0; row < rows; row++)
-    for (col=0; col < cols; col++) {
+  { for (col=0; col < cols; col++) {
       PIXEL* o = original + row*cols + col;
-      PIXEL* n = (*new) + row*cols + (cols-1-col);
+      //PIXEL* n = (*new) + row*cols + (cols-1+row);
+      PIXEL* n = (*new) + row * cols + (cols-1-col);// * rows+row;
       *n = *o;
-    }
-
+ }
+  }
   return 0;
 }
+
+
 /*
  * This method horizontally flips a 24-bit, uncompressed bmp file
  * that has been read in using readFile().
@@ -274,22 +262,24 @@ int verticalflip (PIXEL *original, PIXEL **new, int rows, int cols)
  *
  * new      - the new array containing the PIXELs, allocated within
  */
-int flip (PIXEL *original, PIXEL **new, int rows, int cols)
+int flip (PIXEL *original,PIXEL **new, int rows, int cols)
 {
   int row;
   int col;
 
-  if ((rows <= 0) || (cols <= 0)) return -1;
-
+  if((rows <= 0) || (cols <= 0)) return -1;
+  //*newrows = rows;
+  //*newcols = cols;
   *new = (PIXEL*)malloc(rows*cols*sizeof(PIXEL));
   
   for (row =0; row < rows; row++)
-    for(col = 0; col<cols; col++)
+    for(col = 0; col < cols; col++)
     {
-      PIXEL * o= original + row * cols + col;
-      PIXEL* n = (*new) + (cols - col - 1) * cols + row;
+      PIXEL* o = original + row * cols + col;
+      PIXEL* n = (*new) + (cols-col-1)* cols + row;//(cols-1-col);// + col;//(cols-1-col);
       *n = *o;
-    }
+    } 
+  
   return 0;
 }
 
