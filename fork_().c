77da34880a7041ad7699d97288cc3374b22d9dc4
qubitAI <osmanmorales@ymail.com>
2018-11-24 16:51:58 -0500

    Update fork_().c

--- fork_().c
+++ fork_().c
@@ -27,10 +27,8 @@ void check_circuit (int id, int z)
           v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7],
           v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15]);
   }
-   
-
-
-
+  
+  fflush(stdout);
 }
 
 //constants
