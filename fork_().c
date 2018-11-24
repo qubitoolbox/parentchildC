dd48a986ac899d5b55081c6a3806345b27b51541
qubitAI <osmanmorales@ymail.com>
2018-11-24 16:51:22 -0500

    Update fork_().c

--- fork_().c
+++ fork_().c
@@ -23,6 +23,9 @@ void check_circuit (int id, int z)
       && (v[12] || v[13]) && (v[13] || !v[14])
       && (v[14] || v[15])))
   {
+    prinf("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", id,
+          v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7],
+          v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15]);
   }
    
 
