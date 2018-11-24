763575d58b81ba5af53b29b0dc15ba17fe3b1747
qubitAI <osmanmorales@ymail.com>
2018-11-24 16:38:17 -0500

    Update fork_().c

--- fork_().c
+++ fork_().c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+/*Return 1 if i'th bit of n is 1; 0 otherwise*/
+
+#define EXTRACT_BIT(n,i) ((n&(1<<i))?1:0)
+
+void check_circuit (int id, int z)
+{
+  int v[16] ; //each element is a bit of z
+   
+
+
+
+}
+
 //constants
 typedef int bool;
 #define true 1;
